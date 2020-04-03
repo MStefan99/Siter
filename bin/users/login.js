c9f726ddb626a68f22dec6d51a002892f09e49b4
Mikhail Stefantsev <mikhail.stefantsev@outlook.com>
2020-04-03 13:43:33 +0300

    added about page
    fixed navigation panel links
    replaced index page with dashboard

--- bin/users/login.js
+++ bin/users/login.js
@@ -12,20 +12,45 @@ router.use(bodyParser.urlencoded({extended: true}));
 router.use(cookieParser());
 
 
-const createUserTable = (db) => {
-	db.prepare('create table users\
-					(\
-					id integer\
-					constraint users_pk\
-					primary key autoincrement,\
-					username text not null,\
-					pass_hash text not null,\
-					cookie_id text\
-					);').run();
-	db.prepare('create unique index users_id_uindex\
-				on users (id);').run();
-	db.prepare('create unique index users_username_uindex\
-				on users (username);').run();
+const initUserTables = (db) => {
+	// Creating table for users
+	db.prepare(`create table users (
+						id integer not null
+					constraint users_pk
+					primary key autoincrement,
+						username text not null,
+						password_hash text,
+						setup_code text,
+						is_admin integer default 0 not null,
+						can_manage_users integer default 0 not null
+				);
+				`).run();
+	db.prepare(`create unique index users_id_uindex
+					on users (id);`).run();
+	db.prepare(`create unique index users_setup_code_uindex
+					on users (setup_code);`).run();
+	db.prepare(`create unique index users_username_uindex
+					on users (username);`).run();
+
+	// Creating table for user sessions
+	db.prepare(`create table sessions (
+					id integer
+				constraint sessions_pk
+				primary key autoincrement,
+					user_id integer not null
+				constraint user_id___fk
+				references users
+				on update restrict,
+					cookie_id text not null,
+					ip_address text not null,
+					os text not null,
+					last_login text not null
+				);
+			`).run();
+	db.prepare(`create unique index sessions_id_uindex
+					on sessions (id);`).run();
+	db.prepare(`create unique index sessions_user_id_uindex
+					on sessions (user_id);`).run();
 };
 
 
@@ -34,14 +59,14 @@ router.post('/login', (req, res) => {
 
 	const tables = db.prepare('select name \
 							 from sqlite_master \
-						 	 where type=\'table\' \
+							 where type=\'table\' \
 							 and name=\'users\';').all();
-	if (!tables.length) {  // creating the table if it's not present
-		createUserTable(db);
+	if (!tables.length) {
+		initUserTables(db);  // creating the table if it's not present
 	}
 	const users = db.prepare('select *\
 							  from users\
-						      where username = ?').get(req.body.username);
+							  where username = ?').get(req.body.username);
 	if (!users) {
 		res.send('No users added! Please ask the owner of this server to add at least one user.');
 	}
--- bin/www/pages.js
+++ bin/www/pages.js
@@ -1,13 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const cookieParser = require('cookie-parser');
 
 
+router.use(cookieParser());
+
+// general pages
 router.get('/', (req, res) => {
-	res.render('index', {main: "main"});
+	if (!req.cookies.siterID) {
+		res.redirect('/login/', 302);
+	} else {
+		res.redirect('/dashboard/', 302);
+	}
 });
 
+router.get('/about', (req, res) => {
+	res.render('about');
+});
+
+// user pages
 router.get('/login', (req, res) => {
 	res.render('user/login');
+});
+
+// control pages
+router.get('/dashboard', (req, res) => {
+	res.render('controls/dashboard');
 });
 
 
