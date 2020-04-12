39353fdba534fed85cdde98d96f46d2ee7eaa4d2
Mikhail Stefantsev <stephan05.08.67@gmail.com>
2020-04-12 19:36:06 +0300

    migrated to sqlite module

--- bin/users/login.js
+++ bin/users/login.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const Database = require('better-sqlite3');
+const sqlite = require('sqlite');
+const sqlite3 = require('sqlite3');
 const pbkdf2 = require('pbkdf2');
 const uuid = require('uuid');
 
@@ -12,67 +13,75 @@ router.use(bodyParser.urlencoded({extended: true}));
 router.use(cookieParser());
 
 
-const initUserTables = (db) => {
+const initUserTables = async (db) => {
 	// Creating table for users
-	db.prepare(`create table users (
-						id integer not null
-					constraint users_pk
-					primary key autoincrement,
-						username text not null,
-						password_hash text,
-						setup_code text,
-						is_admin integer default 0 not null,
-						can_manage_users integer default 0 not null
-				);
-				`).run();
-	db.prepare(`create unique index users_id_uindex
-					on users (id);`).run();
-	db.prepare(`create unique index users_setup_code_uindex
-					on users (setup_code);`).run();
-	db.prepare(`create unique index users_username_uindex
-					on users (username);`).run();
+	await db.run(`create table users (
+				id integer not null
+			constraint users_pk
+			primary key autoincrement,
+				username text not null,
+				password_hash text,
+				setup_code text,
+				is_admin integer default 0 not null,
+				can_manage_users integer default 0 not null
+			
+			create unique index users_id_uindex
+				on users (id);
+			
+			create unique index users_setup_code_uindex
+			on users (setup_code);
+			
+			create unique index users_username_uindex
+			on users (username);
+			`);
 
 	// Creating table for user sessions
-	db.prepare(`create table sessions (
-					id integer
-				constraint sessions_pk
-				primary key autoincrement,
-					user_id integer not null
-				constraint user_id___fk
-				references users
-				on update restrict,
-					cookie_id text not null,
-					ip_address text not null,
-					os text not null,
-					last_login text not null
-				);
-			`).run();
-	db.prepare(`create unique index sessions_id_uindex
-					on sessions (id);`).run();
-	db.prepare(`create unique index sessions_user_id_uindex
-					on sessions (user_id);`).run();
+	await db.run(`create table sessions (
+				id integer
+			constraint sessions_pk
+			primary key autoincrement,
+				user_id integer not null
+			constraint user_id___fk
+			references users
+			on update restrict,
+				cookie_id text not null,
+				ip_address text not null,
+				os text not null,
+				last_login text not null
+			);
+			
+			create unique index sessions_id_uindex
+				on sessions (id);
+			
+			create unique index sessions_user_id_uindex
+				on sessions (user_id);`);
 };
 
 
-router.post('/login', (req, res) => {
-	const db = new Database('./database/users.sqlite');
+router.post('/login', async (req, res) => {
+	await sqlite.open({
+		filename: 'database/users.sqlite',
+		driver: sqlite3.Database
+	}).then(async db => {
+		const tables = await db.all('select name \
+								from sqlite_master \
+								where type=\'table\' \
+								and name=\'users\';');
+		if (!tables.length) {
+			await initUserTables(db);  // creating the table if it's not present
+		}
+		const users = await db.get('select *\
+											from users\
+											where username = $un', {$un: req.body.username});
+		if (!users) {
+			res.send('User not found! Please check if you have access to this server');
+		}
 
-	const tables = db.prepare('select name \
-							 from sqlite_master \
-							 where type=\'table\' \
-							 and name=\'users\';').all();
-	if (!tables.length) {
-		initUserTables(db);  // creating the table if it's not present
-	}
-	const users = db.prepare('select *\
-							  from users\
-							  where username = ?').get(req.body.username);
-	if (!users) {
-		res.send('No users added! Please ask the owner of this server to add at least one user.');
-	}
+		await db.close();
+		res.json(users);
+	});
 
-	db.close();
-	res.json(users);
+
 });
 
 
--- bin/www/pages.js
+++ bin/www/pages.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const cookieParser = require('cookie-parser');
 
-
 router.use(cookieParser());
+
 
 // general pages
 router.get('/', (req, res) => {
@@ -14,14 +14,17 @@ router.get('/', (req, res) => {
 	}
 });
 
+
 router.get('/about', (req, res) => {
 	res.render('about');
 });
+
 
 // user pages
 router.get('/login', (req, res) => {
 	res.render('user/login');
 });
+
 
 // control pages
 router.get('/dashboard', (req, res) => {
