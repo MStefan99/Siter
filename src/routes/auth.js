9c600256505022b0cd8a8e050fe43f663c86001e
Mikhail Stefantsev <stephan05.08.67@gmail.com>
2020-10-08 12:46:34 +0300

    added password change

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -31,7 +31,7 @@ router.get('/about', (req, res) => {
 
 
 router.get('/login', (req, res) => {
-	res.render('user/login');
+	res.render('login');
 });
 
 
@@ -69,6 +69,45 @@ router.get('/logout', async (req, res) => {
 
 	res.clearCookie('siterSESSION', cookieOptions)
 	.redirect('/login/');
+});
+
+
+router.post('/settings', async (req, res) => {
+	if (!req.body.oldPassword) {
+		res.flash({
+			title: 'Empty old password',
+			info: 'Please enter your current password to continue',
+			type: 'warning'
+		}).redirect('/settings/');
+	} else if (!req.body.newPassword || !req.body.newPasswordRepeat) {
+		res.flash({
+			title: 'Empty new password',
+			info: 'Please enter your new password to continue',
+			type: 'warning'
+		}).redirect('/settings/');
+	} else if (req.body.newPassword !== req.body.newPasswordRepeat) {
+		res.flash({
+			title: 'Passwords do not match',
+			info: 'Please be careful when typing passwords and try again',
+			type: 'warning'
+		}).redirect('/settings/');
+	} else if (!await libAuth.verifyPassword(req.body.oldPassword)) {
+		res.flash({
+			title: 'Wrong password',
+			info: 'You have entered the wrong password, please try again',
+			type: 'danger'
+		}).redirect('/settings/');
+	} else {
+		await libAuth.setPassword(req.body.newPassword);
+		await libSession.deleteAllSessions();
+
+		res.flash({
+			title: 'Success!',
+			info: 'You can now log in with a new password.' +
+				'You have been logged out on all devices.',
+			type: 'success'
+		}).redirect('/login/');
+	}
 });
 
 
