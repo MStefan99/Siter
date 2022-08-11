a4853683b3fd34f2d46f96bc13365af7e2d52c69
Mikhail Stefantsev <stephan05.08.67@gmail.com>
2022-08-11 10:55:51 +0300

    updated setup script

--- setup.sh
+++ setup.sh
@@ -1,9 +1,27 @@
-#!/bin/sh
+#!/bin/bash
 
-curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-sudo apt-get update
-sudo apt-get install nodejs sqlite3 -y
-npm install
+echo "Checking Node.js version..."
+which node > /dev/null || (echo "Node.js is not installed. Please install version 12 or higher and try again"; exit 1)
+NODE_VERSION=$(node -v)
 
-cat ./database/ddl/*.sql | sqlite3 ./database/db.sqlite
-echo Setup done! Please run \"node ./app.js\" to get started.
+if [[ ${NODE_VERSION:1:2} -lt 12 ]]; then
+	echo "You are using an outdated version of Node. Please install version 12 or higher and try again"
+	exit 1;
+fi
+echo "Node.js installed!"
+
+echo "Installing Stylus..."
+sudo npm i -g stylus
+echo "Compiling styles..."
+npx stylus ./public/style
+echo "Styles compiled!"
+
+echo "Installing dependencies..."
+npm i
+echo "Dependencies installed!"
+
+echo "Building..."
+npx webpack build
+echo "Build complete!"
+
+echo "Installed successfully! Run \"node app.js\" to start Siter"
