357865968097ac7136632a9084da0e2254476e6d
Mikhail Stefantsev <mikhail.stefantsev@outlook.com>
2023-03-22 22:04:33 +0200

    replaced mongo host with ip

--- backend/lib/db.js
+++ backend/lib/db.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const {MongoClient} = require('mongodb');
-const url = 'mongodb://localhost:27017';
+const url = 'mongodb://127.0.0.1:27017';
 const client = new MongoClient(url);
 
 const dbPromise = client.connect().then(conn => conn.db('siter'));
