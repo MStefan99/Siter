30864418ac0587e0f2256f5b1fefef71365eba81
Mikhail Stefantsev <mikhail.stefantsev@outlook.com>
2023-04-02 13:49:11 +0300

    fixed submitting metrics

--- backend/lib/init.js
+++ backend/lib/init.js
@@ -1,27 +1,23 @@
 'use strict';
 
-const db = require('./db');
 const auth = require('./auth');
 const metrics = require('./metrics');
 const appManager = require('./app_manager');
 
 
 async function init() {
-	auth.init();
+	await auth.init();
 
 	setInterval(async () => {
 		const analytics = appManager.getAnalyticsOptions();
+		const collectedMetrics = await metrics.collect();
+		analytics.enabled && await metrics.submit(collectedMetrics, analytics.url, analytics.telemetryKey);
+
 		const apps = appManager.getApps().filter(a => a.analytics.enabled);
-
-		if (analytics.enabled || apps.length) {
-			const collectedMetrics = await metrics.collect();
-			analytics.enabled && await metrics.submit(collectedMetrics, analytics.url, analytics.key);
-
-			for (const app of apps) {
-				app.analytics.metricsEnabled && await metrics.submit(collectedMetrics, app.analytics.url, app.analytics.key);
-			}
+		for (const app of apps) {
+			app.analytics.metricsEnabled && await metrics.submit(collectedMetrics, app.analytics.url, app.analytics.key);
 		}
-	}, 1000 * 30);
+	}, 1000 * 10);
 }
 
 module.exports = {init};
